5618e208ee7ba13a42433465323dd4dfce88b09f
LucasE2996 <moeda.60@gmail.com>
2020-03-01 22:44:32 -0300

    estilizando formulário

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-import {Container} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Container, Form, Input, SubmitButton} from './styles';
 
 const Main = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={14} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 };
 
 Main.navigationOptions = {
